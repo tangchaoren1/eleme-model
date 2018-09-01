f693688bc18b543939951aa85ec94f531b479add
tangchaoren1 <735405280@qq.com>
2018-09-01 15:57:26 +0800

    引入vant

--- web/src/main.js
+++ web/src/main.js
@@ -1,11 +1,14 @@
 
 import Vue from 'vue';
-import iView from 'iview';
+// import iView from 'iview';
 import App from './App.vue';
 import { router } from './router/index';
-import 'iview/dist/styles/iview.css';
+import Vant from 'vant';
+import 'vant/lib/vant-css/index.css';
+Vue.use(Vant);
+// import 'iview/dist/styles/iview.css';
 import axios from 'axios';
-Vue.use(iView);
+// Vue.use(iView);
 Vue.prototype.$http = axios;
 new Vue({
     el: '#app',
